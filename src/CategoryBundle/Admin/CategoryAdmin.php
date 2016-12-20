5f937f3c286b2d78c418a84aaeab427b4a0fed63
kostya <kostya_nagula@mail.ua>
2016-12-21 00:40:29 +0200

    create Category Entity and migration for CateforyEntity

---
+++ src/CategoryBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kostya
+ * Date: 21.12.16
+ * Time: 0:20
+ */
+namespace CategoryBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', 'text');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+}
