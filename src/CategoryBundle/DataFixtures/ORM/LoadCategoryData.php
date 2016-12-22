e28a368618703b014d80970e56b96409af3c5b5d
kostya <kostya_nagula@mail.ua>
2016-12-22 17:17:24 +0200

    create fixture LoadCategoryData

---
+++ src/CategoryBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kostya
+ * Date: 22.12.16
+ * Time: 17:10
+ */
+namespace CategoryBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CategoryBundle\Entity\Category;
+
+/**
+ * Class LoadCategoryData are used to load a set of category data into database,
+ * and used for testing or could be initial data required for the application to run smoothly.
+ *
+ * @package CategoryBundle\DataFixtures\ORM
+ */
+class LoadCategoryData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * Allow you to run creating fixture
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $phone = new Category();
+        $phone->setName('phone');
+        $manager->persist($phone);
+        
+        $notebook = new Category();
+        $notebook->setName('notebook');
+        $manager->persist($notebook);
+        
+        $manager->flush();
+    }
+
+    /**
+     * allowing to set order in which fixtures are loaded
+     *
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 5;
+    }
+}
