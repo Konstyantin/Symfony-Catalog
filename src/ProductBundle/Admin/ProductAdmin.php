d99543d91d6e36d8c8a51831f58bad7824f94dc8
kostya <kostya_nagula@mail.ua>
2016-12-22 18:02:44 +0200

    create ProductAdmin

---
+++ src/ProductBundle/Admin/ProductAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kostya
+ * Date: 22.12.16
+ * Time: 17:45
+ */
+namespace ProductBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class ProductAdmin
+ * @package ProductBundle\Admin
+ */
+class ProductAdmin extends AbstractAdmin
+{
+    /**
+     * Configure fields which are displayed on the edit and create actions
+     *
+     * @param FormMapper $formMapper
+     */
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name','text',['help' => 'Enter name product',])
+            ->add('price','text',['help' => 'Enter price product'])
+            ->add('description','textarea',['help' => 'Enter description product']);
+    }
+
+    /**
+     * Configure the filters, used to filter and sort the list of models
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('name')
+            ->add('price');
+    }
+
+    /**
+     * Specific fields which are show when all model are listed
+     *
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('name')
+            ->add('price')
+            ->add('description');
+    }
+
+    /**
+     * This receives the object to transform to a string as the first parameter
+     *
+     * @param mixed $object
+     * @return string
+     */
+    public function toString($object)
+    {
+        if($object instanceof Category){
+            return $object->getName();
+        }
+
+        return 'Product'; // shown in the breadcrumb on the create view
+    }
+}
