cfa3d47a183a02fca3014c1e830e9c6c4a9aaa5c
kostya <kostya_nagula@mail.ua>
2016-12-25 18:05:02 +0200

    create Category repository method

--- src/CategoryBundle/Repository/CategoryRepository.php
+++ src/CategoryBundle/Repository/CategoryRepository.php
@@ -10,4 +10,35 @@ namespace CategoryBundle\Repository;
  */
 class CategoryRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * Get all exists categories
+     * 
+     * @return array
+     */
+    public function getAllCategory()
+    {
+        $query = $this->createQueryBuilder('c')
+            ->getQuery();
+
+        $category = $query->getResult();
+
+        return $category;
+    }
+    
+    /**
+     * Get category id by set name
+     * 
+     * @param $name
+     * @return int
+     */
+    public function getCategoryId($name)
+    {
+        $product = $this->getEntityManager()
+            ->getRepository('CategoryBundle:Category')
+            ->findOneBy(['name' => $name]);
+        
+        $product_id = $product->getId();
+
+        return $product_id;
+    }
 }
